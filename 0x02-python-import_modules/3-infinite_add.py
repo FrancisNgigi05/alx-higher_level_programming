6a45cca692a6b9de28b4a43e6a9dee916fab387d
toxicanonymous253 <ngugifrancis930@gmail.com>
2024-03-03 13:33:47 +0300

    task

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    argv_length = len(sys.argv)
+    add = 0
+
+    if argv_length < 2:
+        print(0)
+
+    for i in range(1, argv_length):
+        add += int(sys.argv[i])
+    print("{}".format(add))
