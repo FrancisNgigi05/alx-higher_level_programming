94eeba97f710d378ccc64987a85d927cba04da62
toxicanonymous253 <ngugifrancis930@gmail.com>
2024-03-03 14:47:08 +0300

    task

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import hidden_4
+
+    modules = dir(hidden_4)
+    
+    for function in modules:
+        if not function.startswith("__"):
+            print(function)
+
