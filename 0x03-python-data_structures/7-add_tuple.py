a3ebd53c223ef39ee9bd78157fcf013de2aaeb8b
toxicanonymous253 <ngugifrancis930@gmail.com>
2024-03-05 13:08:14 +0300

    task

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    list_a = list(tuple_a)
+    list_b = list(tuple_b)
+
+    for number in range(len(list_a), 2):
+        if len(list_a) < 2:
+            list_a.append(0)
+
+    for integer in range(len(list_b), 2):
+        if len(list_b) < 2:
+            list_b.append(0)
+
+    result = [(list_a[0] + list_b[0]), (list_b[1] + list_b[1])]
+
+    new_tuple = tuple(result)
+
+    return result
