2a07d38abcbdfe969508b67a3c5ff1de072a65d7
toxicanonymous253 <ngugifrancis930@gmail.com>
2024-03-05 20:26:00 +0300

    task

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,12 +2,7 @@
 
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        return None
-
-    first_letter = sentence[0]
-
-    sentence_len = len(sentence)
-
-    result = (sentence_len, first_letter)
+        return (None,)
+    result = (len(sentence), sentence[0])
 
     return result
