865c6a4aa6f1b65102c13baae3cc8dad62871f8d
toxicanonymous253 <ngugifrancis930@gmail.com>
2024-03-07 20:37:59 +0300

    task

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """Represents a square"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not (isinstance(value, tuple) or len(value) != 2 or not not all(isinstance(i, int) for i in value) or not all(i >= 0 for i in value)):
+            raise TypeError("positive must be an integer or a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            for space in range(self.__position[1]):
+                print("_")
+            for symbol in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
