1e3977527d11548822332854c4005f99776644a0
toxicanonymous253 <ngugifrancis930@gmail.com>
2024-03-15 13:40:34 +0300

    task

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -34,14 +34,14 @@ class Rectangle:
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value  < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
 
     def area(self):
         return self.__width * self.__height
-    
+
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return 0
@@ -61,10 +61,9 @@ class Rectangle:
 
         return "".join(rect)
 
-
     def __repr__(self):
         return "Rectangle({}, {})".format(self.__width, self.__height)
 
     def __del__(self):
         type(self).number_of_instances -= 1
-        print("Bye Rectangle...")
+        print("Bye rectangle...")
